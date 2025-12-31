2f688f24f0fe57245a6fed09b7bf3c326f8ee36a
Atta Navaid <a302@umbc.edu>
2025-12-31 01:54:12 -0500

    refactor skillsData to include detailed technology objects with SVG icons for improved clarity and organization

--- data/skillsData.ts
+++ data/skillsData.ts
@@ -1,90 +1,151 @@
-import type { Skill } from '@/types';
+import type { Skill } from "@/types";
 
 export const skillsData: Skill[] = [
-    {
-        icon: "/skills/web.png",
-        name: "Website Development and Design",
-        tech: ["HTML", "CSS", "TypeScript", "JavaScript"]
-    },
-    {
-        icon: "/skills/react.png",
-        name: "Front-End Frameworks",
-        tech: ["React", "Vue", "Gatsby", "jQuery"]
-    },
-    {
-        icon: "/skills/nodejs.png",
-        name: "Back-End Frameworks",
-        tech: ["Node", "Flask", "Django", "NextJS"]
-    },
-    {
-        icon: "/skills/bootstrap.png",
-        name: "CSS Frameworks",
-        tech: ["Bootstrap", "Tailwind", "Bulma", "Foundation"]
-    },
-    {
-        icon: "/skills/git.png",
-        name: "Development Tools",
-        tech: ["Git", "Jira", "Vite", "XAMPP"]
-    },
-    {
-        icon: "/skills/netlify.png",
-        name: "Deployment Services",
-        tech: ["Netlify", "Vercel", "Render", "GitHub Pages"]
-    },
-    {
-        icon: "/skills/openai.png",
-        name: "API Development",
-        tech: ["OpenAI", "Google OAuth 2.0"]
-    },
-    {
-        icon: "/skills/python.png",
-        name: "Python Scripting",
-        tech: ["Python 3", "Anaconda", "Jupyter Notebook"]
-    },
-    {
-        icon: "/skills/hardhat.png",
-        name: "Web 3.0 Development",
-        tech: ["Solidity", "HardHat", "Alchemy", "MetaMask"]
-    },
-    {
-        icon: "/skills/database.png",
-        name: "Database Management System",
-        tech: ["SQL", "SQLite3", "MySQL", "phpMyAdmin"]
-    },
-    {
-        icon: "/skills/blender.png",
-        name: "3D PBR and Lowpoly Modeling",
-        tech: ["Blender"]
-    },
-    {
-        icon: "/skills/substance.png",
-        name: "3D Texturing and Painting",
-        tech: ["Substance Painter", "Marmoset"]
-    },
-    {
-        icon: "/skills/illustrator.png",
-        name: "UI/UX Design",
-        tech: ["Material UI", "Adobe Illustrator", "Figma"]
-    },
-    {
-        icon: "/skills/tensorflow.png",
-        name: "Machine Learning",
-        tech: ["Scikit-Learn", "TensorFlow", "Keras"]
-    },
-    {
-        icon: "/skills/photoshop.png",
-        name: "Photo Manipulation and Editing",
-        tech: ["Adobe Photoshop", "GIMP"]
-    },
-    {
-        icon: "/skills/premierepro.png",
-        name: "Video Editing and Post-Processing",
-        tech: ["Adobe Premiere Pro", "Adobe After Effects"]
-    },
-    {
-        icon: "/skills/roblox.png",
-        name: "Game Development",
-        tech: ["Unity", "Unreal Engine", "Roblox Studio", "Lua"]
-    }
+  {
+    icon: "/skills/web.png",
+    name: "Web Development",
+    tech: [
+      { name: "HTML", logo: "/skills/tech/html.svg" },
+      { name: "CSS", logo: "/skills/tech/css.svg" },
+      { name: "TypeScript", logo: "/skills/tech/typescript.svg" },
+      { name: "JavaScript", logo: "/skills/tech/javascript.svg" },
+    ],
+  },
+  {
+    icon: "/skills/react.png",
+    name: "Front-End Frameworks",
+    tech: [
+      { name: "React", logo: "/skills/tech/react.svg" },
+      { name: "NextJS", logo: "/skills/tech/nextjs.svg" },
+      { name: "Vue", logo: "/skills/tech/vue.svg" },
+      { name: "jQuery", logo: "/skills/tech/jquery.svg" },
+    ],
+  },
+  {
+    icon: "/skills/nodejs.png",
+    name: "Back-End Frameworks",
+    tech: [
+      { name: "Node", logo: "/skills/tech/nodejs.svg" },
+      { name: "Express", logo: "/skills/tech/express.svg" },
+      { name: "Flask", logo: "/skills/tech/flask.svg" },
+      { name: "Django", logo: "/skills/tech/django.svg" },
+    ],
+  },
+  {
+    icon: "/skills/tailwind.png",
+    name: "CSS Frameworks",
+    tech: [
+      { name: "Tailwind", logo: "/skills/tech/tailwindcss.svg" },
+      { name: "Bootstrap", logo: "/skills/tech/bootstrap.svg" },
+      { name: "Material UI", logo: "/skills/tech/mui.svg" },
+      { name: "Sass", logo: "/skills/tech/sass.svg" },
+    ],
+  },
+  {
+    icon: "/skills/git.png",
+    name: "Development Tools",
+    tech: [
+      { name: "Git", logo: "/skills/tech/git.svg" },
+      { name: "Vite", logo: "/skills/tech/vite.svg" },
+      { name: "Cursor", logo: "/skills/tech/cursor.svg" },
+      { name: "XAMPP", logo: "/skills/tech/xampp.svg" },
+    ],
+  },
+  {
+    icon: "/skills/rapidapi.png",
+    name: "API Development",
+    tech: [
+      { name: "RESTful APIs", logo: "/skills/tech/restapi.svg" },
+      { name: "RapidAPI", logo: "/skills/tech/rapid.svg" },
+      { name: "GraphQL", logo: "/skills/tech/graphql.svg" },
+    ],
+  },
+  {
+    icon: "/skills/clerk.png",
+    name: "Authentication",
+    tech: [
+      { name: "Clerk", logo: "/skills/tech/clerk.svg" },
+      { name: "Google OAuth 2.0", logo: "/skills/tech/google.svg" },
+    ],
+  },
+  {
+    icon: "/skills/vercel.png",
+    name: "Deployment Services",
+    tech: [
+      { name: "Vercel", logo: "/skills/tech/vercel.svg" },
+      { name: "GitHub Pages", logo: "/skills/tech/github.svg" },
+      { name: "Netlify", logo: "/skills/tech/netlify.svg" },
+      { name: "Render", logo: "/skills/tech/render.svg" },
+    ],
+  },
+  {
+    icon: "/skills/hardhat.png", // TODO: Change to the correct icon
+    name: "Web 3.0 Development",
+    tech: [
+      { name: "Solidity", logo: "/skills/tech/solidity.svg" },
+      { name: "HardHat", logo: "/skills/tech/hardhat.svg" },
+      { name: "Alchemy", logo: "/skills/tech/alchemy.svg" },
+      "MetaMask",
+    ],
+  },
+  {
+    icon: "/skills/database.png",
+    name: "Database Management System",
+    tech: [
+      { name: "SQL", logo: "/skills/tech/mysql.svg" },
+      { name: "SQLite3", logo: "/skills/tech/sqlite.svg" },
+      { name: "MongoDB", logo: "/skills/tech/mongodb.svg" },
+      { name: "PrismaORM", logo: "/skills/tech/prisma.svg" },
+    ],
+  },
+  {
+    icon: "/skills/blender.png",
+    name: "3D PBR and Lowpoly Modeling",
+    tech: [{ name: "Blender", logo: "/skills/tech/blender.svg" }],
+  },
+  {
+    icon: "/skills/substance.png",
+    name: "3D Texturing and Painting",
+    tech: [
+      { name: "Substance Painter", logo: "/skills/tech/substance.svg" },
+      { name: "Marmoset", logo: "/skills/tech/marmoset.svg" },
+    ],
+  },
+  {
+    icon: "/skills/photoshop.png", // TODO: Change to the correct icon
+    name: "UI/UX Design",
+    tech: [
+      { name: "Adobe Photoshop", logo: "/skills/tech/adobe-photoshop.svg" },
+      { name: "Figma", logo: "/skills/tech/figma.svg" },
+      { name: "Adobe Illustrator", logo: "/skills/tech/adobe-illustrator.svg" },
+    ],
+  },
+  {
+    icon: "/skills/tensorflow.png",
+    name: "Machine Learning",
+    tech: [
+      { name: "Scikit-Learn", logo: "/skills/tech/scikitlearn.svg" },
+      { name: "TensorFlow", logo: "/skills/tech/tensorflow.svg" },
+      { name: "Keras", logo: "/skills/tech/keras.svg" },
+      { name: "Jupyter Notebook", logo: "/skills/tech/jupyter.svg" },
+    ],
+  },
+  {
+    icon: "/skills/premierepro.png",
+    name: "Video Editing and Post-Processing",
+    tech: [
+      { name: "Adobe Premiere Pro", logo: "/skills/tech/adobe-premierepro.svg" },
+      { name: "Adobe After Effects", logo: "/skills/tech/adobe-aftereffects.svg" },
+    ],
+  },
+  {
+    icon: "/skills/roblox.png",
+    name: "Game Development",
+    tech: [
+      { name: "Roblox Studio", logo: "/skills/tech/robloxstudio.svg" },
+      { name: "Lua", logo: "/skills/tech/lua.svg" },
+      { name: "Unity", logo: "/skills/tech/unity.svg" },
+      { name: "Unreal Engine", logo: "/skills/tech/unrealengine.svg" },
+    ],
+  },
 ];
-
